55656a4aa35a24efdd3659ad9a6d54ca5eacdf31
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-09-05 19:45:35 -0300

    Criação da aplicação e configuração do Styled Components

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/global/styles/theme";
+
+export default function App() {
+  return <ThemeProvider theme={theme}></ThemeProvider>;
+}
